853cffa3e148fdcc0ac05212eafc8ed615186828
Marcello De Giorgi <marcello.degiorgi4@studio.unibo.it>
2019-04-09 17:30:54 +0200

    Progetto creato. Ho importato tutte le classi di cui abbiamo bisogno. Non so se alla competizione sono da usare però! Da vedere.

---
+++ TablutAICode/src/client/TablutAIClient.java
@@ -0,0 +1,20 @@
+package client;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+public class TablutAIClient extends TablutClient {
+	
+
+	public TablutAIClient(String player, String name) throws UnknownHostException, IOException {
+		super(player, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
