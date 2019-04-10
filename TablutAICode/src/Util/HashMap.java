2869148b9fbe5c84b9d0c0a67f41578370a8d159
Alessandro Perrone <alessandro.perrone8@studio.unibo.it>
2019-04-10 15:40:20 +0200

    Ale: Aggiunto il package Util con le classi HashMap PawnClass Position

---
+++ TablutAICode/src/Util/HashMap.java
@@ -0,0 +1,49 @@
+package Util;
+
+import java.util.Map;
+
+import domain.State;
+import domain.State.Pawn;
+
+public class HashMap {
+private static HashMap instance;
+	
+	private Map<Position, PawnClass> map = new java.util.HashMap<Position, PawnClass>();
+	
+	private HashMap() {
+		
+	}
+	
+	public static HashMap getInstance() {
+		if(instance == null) 
+			instance = new HashMap();
+		return instance;
+	}
+	
+	public void createMap(State state) {
+		if(!map.isEmpty()) {
+			map.clear();
+		}
+		for(int i = 0; i < state.getBoard().length; i++) {
+			for(int j = 0; j < state.getBoard()[0].length; j++) {
+				switch(state.getBoard()[i][j]) {
+				case WHITE:
+					map.put(new Position(i,j), new PawnClass(i, j, Pawn.WHITE));
+					break;
+				case BLACK:
+					map.put(new Position(i,j), new PawnClass(i, j, Pawn.BLACK));
+					break;
+				case KING:
+					map.put(new Position(i,j), new PawnClass(i, j, Pawn.KING));
+				}
+			}
+		}
+	}
+
+	
+	public void printMap() {
+		map.forEach((position, pawn) -> System.out.println(pawn.toString() + "In position "+position.toString()));
+	}
+	
+	
+}
---
+++ TablutAICode/src/Util/PawnClass.java
@@ -0,0 +1,52 @@
+package Util;
+
+import domain.State.Pawn;
+
+public class PawnClass {
+	private int row, column;
+	private Pawn type;
+	/*
+	 * color: b = black w = white
+	 */
+
+	public PawnClass(int row, int column, Pawn type) {
+		this.row = row;
+		this.column = column;
+		this.type = type;
+	}
+
+	/* METODI PER MUOVERE LA PEDINA */
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	public Pawn getType() {
+		return type;
+	}
+
+	public void setType(Pawn type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Pawn "+type.toString();
+	}
+	
+	
+
+	
+}
